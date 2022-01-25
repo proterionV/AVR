f120925726f699bca88f836e90bba18747cb3504
Igor Abramov <visorak7777@gmail.com>
2022-01-25 13:24:27 +0200

    + Add Repository for devices with AVR
    Repository for AVR devices of Technical Department of SKS Textile EE

---
+++ A2560_Autoassembling/main.c
@@ -0,0 +1,16 @@
+/*
+ * main.c
+ *
+ * Created: 1/25/2022 1:10:22 PM
+ *  Author: igor.abramov
+ */ 
+
+#include <xc.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
