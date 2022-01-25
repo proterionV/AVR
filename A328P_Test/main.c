f0cdb7a3aec6ce47f9f382ca260d955b7e478713
Igor Abramov <visorak7777@gmail.com>
2022-01-25 14:39:19 +0200

    + Add A328P Test Project

---
+++ A328P_Test/main.c
@@ -0,0 +1,16 @@
+/*
+ * main.c
+ *
+ * Created: 1/25/2022 2:34:40 PM
+ *  Author: igor.abramov
+ */ 
+
+#include <xc.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
