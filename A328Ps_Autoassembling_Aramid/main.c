6c0bda6179aac10c0f0ac39fe9ef4b85350b0272
Igor Abramov <visorak7777@gmail.com>
2022-07-08 15:25:43 +0300

    Display is not needed. Well test on table with simplest system.

--- A328Ps_Autoassembling_Aramid/main.c
+++ A328Ps_Autoassembling_Aramid/main.c
@@ -73,57 +73,6 @@ ISR(TIMER2_OVF_vect)
 	TCNT2 = 5;
 }
 
-void EraseUnits(int x, int y, int offset, float count)
-{
-	char eraser = 32;
-
-	if (count<10000)
-	{
-		lcd_gotoxy(x+offset+4,y);
-		lcd_putc(eraser);
-	}
-	
-	if (count<1000)
-	{
-		lcd_gotoxy(x+offset+3,y);
-		lcd_putc(eraser);
-	}
-	
-	if (count<100)
-	{
-		lcd_gotoxy(x+offset+2,y);
-		lcd_putc(eraser);
-	}
-	
-	if (count<10)
-	{
-		lcd_gotoxy(x+offset+1,y);
-		lcd_putc(eraser);
-	}
-	
-	
-	lcd_gotoxy(x, y);
-}
-
-void DisplayPrint()
-{
-	//static char A[20] = { 0 }, P[20] = { 0 };
-	
-	//EraseUnits(0, 0, 3, Measure.Fa);
-	//sprintf(A, "%.2f", Measure.Fa < 0 ? 0 : Measure.Fa);
-	//lcd_puts(A);
-	//
-	//EraseUnits(0, 1, 3, Measure.Fp);
-	//sprintf(P, "%.2f", Measure.Fp < 0 ? 0 : Measure.Fp);
-	//lcd_puts(P);
-	
-	//if (Mode.alarm)
-	//{
-		//lcd_clrline(9, 1);
-		//lcd_puts("Alarm");
-	//}
-}
-
 void Initialization(const char* name)
 {
 	DDRB = 0b00111111;
@@ -132,18 +81,8 @@ void Initialization(const char* name)
 	DDRC = 0b00111111;
 	PORTC = 0b11000000;
 	
-	DDRD = 0b11100011;
+	DDRD = 0b11100000;
 	PORTD = 0b00011100;
-	
-	lcd_init(LCD_DISP_ON);
-	lcd_led(false);
-	lcd_clrscr();
-	lcd_home();
-	
-	lcd_puts(name);
-	_delay_ms(2000);
-	lcd_clrscr();
-	lcd_home();
 	
 	Timer2(Counter);
 	sei();
@@ -152,7 +91,7 @@ void Initialization(const char* name)
 void Step(short direction)
 {
 	ImpOn;
-	if (direction == Left)  _delay_ms(1); // 800 us
+	if (direction == Left)  _delay_ms(1); // 1 ms
 	if (direction == Right) _delay_ms(2); // 2 ms
 	ImpOff;
 	_delay_ms(1); // 1 ms
